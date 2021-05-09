533d546ff105e5a811ca9a6fb260f23b3e353645
AlgoDame <chiamakaojiyi@gmail.com>
2021-05-09 02:12:05 +0100

    init commit

--- task2/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response, NextFunction} from 'express';
-const router = Router();
-
-/* GET home page. */
-router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.render('index', { title: 'Express' });
-});
-
-export default router;
